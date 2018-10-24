9aad9d0b142580ad62451af7be1863dabb491ff6
落心 <33619903+LuoxinY@users.noreply.github.com>
2018-10-24 11:56:31 +0800

    修改忽略列表

--- download_html.py
+++ download_html.py
@@ -15,7 +15,7 @@ class HtmlDownloader:
         '''
         try:
             # url=url.encode("utf-8")
-            print("准备下载页面"+url)
+            # print("准备下载页面"+url)
             user_agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
             headers = {'User-Agent': user_agent}
             req_timeout = 20
